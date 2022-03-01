7f56329f791bd37cc79981ce2c84a77b15208e90
Andrew Naumov <andrewnaumow@gmail.com>
2022-03-01 20:53:09 +0300

    task1

---
+++ task1/crawler.py
@@ -0,0 +1,35 @@
+import os
+
+import requests as requests
+import json
+
+if __name__ == '__main__':
+    input_file = 'source_links.txt'
+
+    meta = {}
+
+    with open(input_file, 'r') as in_file:
+
+        i = 0
+        while True:
+            url = in_file.readline()
+            if url == '':
+                break
+
+            response = requests.get(url)
+            decoded_content = response.content.decode("utf-8")
+
+            filename = '../archive/' + str(i) + '.txt'
+
+            os.makedirs(os.path.dirname(filename), exist_ok=True)
+            with open(filename, 'w', errors="ignore") as out_file:
+                out_file.write(decoded_content)
+
+            meta[filename] = url
+
+            i += 1
+
+        json_string = json.dumps(meta, indent=4, sort_keys=True)
+
+        with open("./index.txt", 'w') as index:
+            index.write(str(json_string))
