d562a00abb263146238e5f9bf53899bc72edbe1b
Andrew Naumov <andrewnaumow@gmail.com>
2022-03-17 11:51:27 +0300

    task2: add english support

--- task2/tokens_lemmas.py
+++ task2/tokens_lemmas.py
@@ -4,13 +4,15 @@ import string
 from functools import reduce
 from os import walk
 
-import nltk
 from nltk.corpus import stopwords
 from pymystem3 import Mystem
+from nltk.stem import WordNetLemmatizer
+import nltk
 
 
 def tokenize(content):
     russian_stopwords = stopwords.words('russian')
+    english_stopwords = stopwords.words('english')
     letters_pattern = re.compile("^[a-zA-Zа-яА-я]+$")
 
     def remove_tags(text):
@@ -30,7 +32,9 @@ def tokenize(content):
         return list(filter(lambda token: token not in string.punctuation, tokens))
 
     def filter_stopwords(tokens):
-        return list(filter(lambda token: token.lower() not in russian_stopwords, tokens))
+        return list(
+            filter(lambda token: token.lower() not in russian_stopwords and token.lower() not in english_stopwords,
+                   tokens))
 
     def filter_trash(tokens):
         return list(filter(letters_pattern.match, tokens))
@@ -46,19 +50,35 @@ def tokenize(content):
 
     return tokens
 
-def group_tokens_by_lemmas(tokens):
+
+def group_tokens_by_lemmas(tokens, eng=False):
     lemma_dict = {}
-    mystem = Mystem()
 
-    # tooooo slow
-    # for token in tokens:
-    #     lemma = mystem.lemmatize(token)[0]
-    #     lemma_dict.setdefault(lemma, []).append(token)
+    lemma_list = []
 
-    # super fast
-    tokens_in_one_line = reduce(lambda a, b: a + '|' + b, tokens)
-    lemma_list = mystem.lemmatize(tokens_in_one_line)
-    lemma_list = list(filter(lambda l: l not in ['|', '\n'], lemma_list))
+    if eng:
+        lemmatizer = WordNetLemmatizer()
+        tagged_tokens: list[tuple[str, str]] = nltk.pos_tag(tokens)
+        for token, tag in tagged_tokens:
+
+            try:
+                lemma = lemmatizer.lemmatize(token, pos=tag[0].lower())
+            except:
+                lemma = lemmatizer.lemmatize(token)
+
+            lemma_list.append(lemma)
+    else:
+        mystem = Mystem()
+
+        # tooooo slow
+        # for token in tokens:
+        #     lemma = mystem.lemmatize(token)[0]
+        #     lemma_dict.setdefault(lemma, []).append(token)
+
+        # super fast
+        tokens_in_one_line = reduce(lambda a, b: a + '|' + b, tokens)
+        lemma_list = mystem.lemmatize(tokens_in_one_line)
+        lemma_list = list(filter(lambda l: l not in ['|', '\n'], lemma_list))
 
     for (token_index, token) in enumerate(tokens):
         lemma = lemma_list[token_index]
@@ -66,9 +86,19 @@ def group_tokens_by_lemmas(tokens):
 
     return lemma_dict
 
+
 def prepare_content(raw_content):
-    result = re.findall(r'<div xmlns="http://www.w3.org/1999/xhtml">((.|\n)*?)<DIV class="v-portal"', raw_content)
-    return result[0][0]
+    if '<div xmlns="http://www.w3.org/1999/xhtml">' in raw_content:
+        result = re.findall(r'<div xmlns="http://www.w3.org/1999/xhtml">((.|\n)*?)<DIV class="v-portal"', raw_content)
+        return result[0][0]
+    if '<div id="mainbar" role="main" aria-label="question and answers">' in raw_content:
+        result = re.findall(
+            r'<div id="mainbar" role="main" aria-label="question and answers">((.|\n)*)?</div>((.|\n)*)?<div id="sidebar" class="show-votes" role="complementary" aria-label="sidebar">',
+            raw_content)
+        result2 = result[0][0]
+        return result2
+    else:
+        return raw_content
 
 
 if __name__ == '__main__':
@@ -100,7 +130,7 @@ if __name__ == '__main__':
 
         # лемматизация
 
-        lemma_dict = group_tokens_by_lemmas(tokens)
+        lemma_dict = group_tokens_by_lemmas(tokens, eng=True)
 
         dir_name = '../lemmas/'
         lemmas_file_prefix = dir_name + 'lemma_'
@@ -109,4 +139,3 @@ if __name__ == '__main__':
         with open(lemmas_file_prefix + filepath, 'w', encoding="utf-8") as lemma_file:
             for lemma, tokens in lemma_dict.items():
                 lemma_file.write(lemma + " " + reduce(lambda s1, s2: s1 + " " + s2, tokens) + '\n')
-
